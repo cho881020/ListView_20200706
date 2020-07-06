5bc4bccf1ea71288f1ca1fc2e76fe0054ff8c18e
조경진 <kj_cho@idealidea.co.kr>
2020-07-06 21:58:09 +0900

    실제 학생 데이터 출력  => 생년을 나이로 바꿔야 함

--- app/src/main/java/kr/co/tjoeun/listview_20200706/adapters/StudentAdapter.kt
+++ app/src/main/java/kr/co/tjoeun/listview_20200706/adapters/StudentAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import kr.co.tjoeun.listview_20200706.R
 import kr.co.tjoeun.listview_20200706.datas.Student
 
@@ -27,6 +28,23 @@ class StudentAdapter(
 //        tempRow에는 null일 가능성이 없다.
 //        진짜 row에는 절대 null이 아니게 된 tempRow를 복사.
         val row = tempRow!!
+
+//        row의 내용을 실제에 맞게 변경
+
+//        (그려줄 위치에 맞는) 실제 데이터를 가져오고
+        val data = mList[position]
+
+//        row의 컴포넌트들을 가져와서
+        val nameTxt = row.findViewById<TextView>(R.id.nameTxt)
+        val addressTxt = row.findViewById<TextView>(R.id.addressTxt)
+        val ageTxt = row.findViewById<TextView>(R.id.ageTxt)
+
+//        데이터 반영
+        nameTxt.text = data.name
+        addressTxt.text = data.address
+
+        ageTxt.text = "${data.birthYear}세"
+
 
 //        이 row를 뿌려달라고 결과로 지정.
         return row
