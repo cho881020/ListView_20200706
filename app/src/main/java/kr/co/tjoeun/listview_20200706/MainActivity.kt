37fa862d4b5571f8267f81828279b4daf3dc0484
조경진 <kj_cho@idealidea.co.kr>
2020-07-07 20:05:14 +0900

    AlertDialog로 진짜 삭제할건지 확인

--- app/src/main/java/kr/co/tjoeun/listview_20200706/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/listview_20200706/MainActivity.kt
@@ -1,8 +1,10 @@
 package kr.co.tjoeun.listview_20200706
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
 import kr.co.tjoeun.listview_20200706.adapters.StudentAdapter
 import kr.co.tjoeun.listview_20200706.datas.Student
@@ -53,11 +55,23 @@ class MainActivity : AppCompatActivity() {
 //        학생 목록 롱클릭 이벤트 처리
         studentListView.setOnItemLongClickListener { parent, view, position, id ->
 
+//            정말 그 사람을 지울지 확인. => 확인 OK 여야 삭제
+            val deleteAlert = AlertDialog.Builder(this)
+            deleteAlert.setTitle("학생 목록 삭제")
+            deleteAlert.setMessage("정말 이 학생을 삭제하시겠습니까?")
+            deleteAlert.setPositiveButton("확인", DialogInterface.OnClickListener { dialog, which ->
+//                확인을 누르면 실제로 삭제 처리
+
 //            오래 눌린 사람을 명단에서 삭제.
-            mStudentList.removeAt(position)
+                mStudentList.removeAt(position)
 
 //            어댑터에게 새로고침 시키기
-            mAdapter.notifyDataSetChanged()
+                mAdapter.notifyDataSetChanged()
+
+            })
+            deleteAlert.setNegativeButton("취소", null)
+            deleteAlert.show()
+
 
 //            LongClick은 Boolean값을 리턴해 줘야 함.
             return@setOnItemLongClickListener true
